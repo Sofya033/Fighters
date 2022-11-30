e1526d0bcfa8f6a785e1ddb9cb380fcff16cbcdd
Sofya033 <113354571+Sofya033@users.noreply.github.com>
2022-11-30 17:48:29 +0500

    Update AbstractFightersClass.php

--- AbstractFightersClass.php
+++ AbstractFightersClass.php
@@ -1,5 +1,8 @@
 <?php
 
 abstract class AbstractFightersClass {
-	abstract public function OutputValues();
+	abstract public function getSpeed() :string;
+	abstract public function getHitPower() :string;
+	abstract public function getProtectionPower() :string;
+	abstract public function getOptions() :string;
 }
